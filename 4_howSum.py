be509824ac95cb5ca1b1e4d0fe3ac5667a93c214
skulu <76804157+skulu@users.noreply.github.com>
2022-03-07 17:24:55 +0800

    Create 4_howSum.py

---
+++ 4_howSum.py
@@ -0,0 +1,30 @@
+# Function returns an array containing any combinantion of elements that add up 
+# to exactly the targetSum. If there is no such combination, return Null. If 
+# multiple combinations are possible, returning any one of them will suffice.
+
+# let n = len(numbers) and m = targetsum
+# O(n^m * m) in time, the extra m is for the copying of the list in line 20, 
+# and O(m) in space before memoisation
+# O(n*m) in time and O(m) in space after memoisation
+
+def howSum(targetSum, numbers, memo = None):
+    if memo is None: memo = {} # don't use mutables in function definition!!
+    if targetSum == 0: return []
+    elif targetSum < 0: return None
+    elif targetSum in memo: return memo[targetSum]
+    
+    for num in numbers:
+        remainder = targetSum - num
+        result =  howSum(remainder, numbers, memo)
+        if result is not None:
+            memo[targetSum] = [num] + result
+            return memo[targetSum]
+    
+    memo[targetSum] = None
+    return None
+
+print(howSum(7,[2,3]))
+print(howSum(7,[5,3,4,7]))
+print(howSum(7,[2,4]))
+print(howSum(8,[2,3,5]))
+print(howSum(300,[7,14]))
