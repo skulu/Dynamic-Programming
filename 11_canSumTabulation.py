aa17e4bf79818027cd57db0e808dda0307403639
skulu <76804157+skulu@users.noreply.github.com>
2022-03-11 23:35:00 +0800

    howSum tabulation formulation

--- 11_canSumTabulation.py
+++ 11_canSumTabulation.py
@@ -2,35 +2,21 @@
 # in the array numbers. Assume all numbers in array are positive and you can 
 # reuse the numbers as many times as you wish.
 
-## 2D array solution ##
-# def canSum(target, numbers):
-#     n = len(numbers)
-#     table = [[0 for col in range(n+1)] for row in range(n+1)]
-    
-#     table[0][1:n+1] = numbers[:]
-#     for i in range(1,n+1):
-#         table[i][0] = numbers[i-1]
-    
-#     for i in range(1,len(table)):
-#         for j in range(1,len(table[0])):
-#             table[i][j] = table[0][j] + table[0][i]
-#             if table[i][j] == target: return True
-
-#     return False
-
-## 1D List solution ##
 # Let n be len(numbers), m be target.
 # O(n*m) time complexity, O(m) space complexity
 def canSum(target, numbers):
     table = [False for col in range(target+1)]
     table[0] = True # base case, then current amount + numbers[x] will also be True
     
-    for i in range(target+1):
+    for i in range(target):
         for j in range(len(numbers)):
             if table[i] == True and (i+numbers[j]) < target+1:
                 table[i+numbers[j]] = True
     
     return table[target]
 
-print(canSum(7,[5,3,4]))
+print(canSum(7,[2,3]))
+print(canSum(7,[5,3,4,7]))
+print(canSum(7,[2,4]))
+print(canSum(8,[2,3,5]))
 print(canSum(300,[7,14])) 
