16973136b0bd5947db3792d9087ccf7e76a91af5
skulu <76804157+skulu@users.noreply.github.com>
2022-03-12 13:05:51 +0800

    fix the zero case

--- 12_howSumTabulation.py
+++ 12_howSumTabulation.py
@@ -1,12 +1,11 @@
 
 def howSum(target, numbers):
     table = [None for col in range(target+1)]
-    
+    table[0] = []
+
     for i in range(target):
         for j in range(len(numbers)):
-            if (table[i] is None) and (numbers[j]) <= target:
-                table[numbers[j]] = [numbers[j]]
-            elif (table[i] is not None):
+            if table[i] is not None:
                 sumWay = table[i] + [numbers[j]]
                 summation = sum(sumWay)
                 if summation <= target and table[summation] is None:
@@ -19,11 +18,3 @@ print(howSum(7,[5,3,4,7]))
 print(howSum(7,[2,4]))
 print(howSum(8,[2,3,5]))
 print(howSum(300,[7,14]))
-
-#%%
-# target = 7
-# table = [1 for col in range(target+1)]
-
-# sum(table)
-
-# %%
